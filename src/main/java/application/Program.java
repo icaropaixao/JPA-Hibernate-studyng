a6f9faafeec9a33f3c56d7ebf318d3f42c43c34e
Ícaro Reis <icaroreis1982@gmail.com>
2025-01-30 21:11:45 -0300

    EntitymanagerFactory and EntityManager criados e conexão com BD bem sucessedida!

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,17 +2,30 @@ package application;
 
 import dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1,"Ícaro","icaro@email");
-        Pessoa p2 = new Pessoa(2,"Nico","Nico@email");
-        Pessoa p3 = new Pessoa(3,"Manu","Manu@email");
+        Pessoa p1 = new Pessoa(null,"Ícaro","icaro@email");
+        Pessoa p2 = new Pessoa(null,"Nico","Nico@email");
+        Pessoa p3 = new Pessoa(null,"Manu","Manu@email");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        // Cria a fábrica de EntityManager usando a unidade de persistência "exemplo-jpa" (definida no persistence.xml).
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();// Cria o EntityManager, responsável por gerenciar operações de banco de dados (CRUD) no JPA.
 
+        em.getTransaction().begin();
+
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+
+        em.getTransaction().commit(); // confimando alterações
+
+        System.out.println("Finish");
     }
 }
